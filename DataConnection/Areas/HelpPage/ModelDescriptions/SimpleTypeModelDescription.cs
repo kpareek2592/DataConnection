9424175f39013b42afc139b57bf01633e02a83f4
Kaushal Pareek <kaushal.pareek@happiestminds.com>
2018-12-10 11:00:13 +0530

    Add project files.

---
+++ DataConnection/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DataConnection.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ DataConnection/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataConnection.WebApiApplication" Language="C#" %>
